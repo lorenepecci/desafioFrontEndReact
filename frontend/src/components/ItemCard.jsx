b5f41fc13426210edd0c573d856a0b673b1b632c
Lorene Pecci <lorenepecci@gmail.com>
2022-06-03 12:47:58 -0300

    css concluded

--- frontend/src/components/ItemCard.jsx
+++ frontend/src/components/ItemCard.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { BsHeart } from "react-icons/bs";
+import { FaRegComment } from "react-icons/fa";
+import {BsCalendar3} from "react-icons/bs";
 
 
 class ItemCard extends React.Component { 
@@ -10,9 +13,9 @@ class ItemCard extends React.Component {
       <div key={ index } >
         <div className="write-card">
           <p>@{ user }</p>
-          <p>{ upvotes }</p>
-          <p>{ comments}</p>
-          <p>{ date.toLocaleTimeString( [], { year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit' } ) }</p>
+          <p><BsHeart/> { upvotes }</p>
+          <p><FaRegComment/> { comments}</p>
+          <p><BsCalendar3/> { date.toLocaleTimeString( [], { year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit' } ) }</p>
         </div>
         <div className="imagem-card">
           <a href={link}>
