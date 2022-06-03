b6f47f6af1581f36a85e48bdbf828c9d7ff40ac8
Lorene Pecci <lorenepecci@gmail.com>
2022-06-03 13:09:09 -0300

    testes RTL concluidos

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,4 +1,4 @@
-/* import { render } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import App from './App';
 import testData from './mockTest/testData';
 
@@ -11,21 +11,31 @@ const mockFetch = () => {
     }));
 };
 
-describe('1 - Faça uma requisição da API e preencha a tela com os dados retornados', () => {
-  beforeAll(mockFetch);
-  beforeEach(cleanup);
+describe( '1 - Faça uma requisição da API e preencha a tela com os dados retornados', () => {
+  beforeAll( mockFetch );
+  beforeEach( cleanup );
 
-  it('Realize uma requisição para a API', async () => {
-    await act(async () => {
-      render(<App />);
-    });
-    expect(global.fetch).toHaveBeenCalled();
-  });
+  it( 'Realize uma requisição para a API', async () => {
+    render( <App /> );
+    expect( global.fetch ).toHaveBeenCalled();
+  } );
 
   it( 'Preencha a tabela com os dados retornados', async () => {
-    await act( async () => {
-      render( <App /> );
-    } );
-    
+    render( <App /> );
+    const img = screen.getByRole('img')
+    expect( img ).toBeInTheDocument();
   } );
-   */
+
+  it( 'existe 1 imagem', async () => {
+    render( <App /> );
+    const ps = await screen.findAllByRole( "img" );
+    expect( ps ).toHaveLength( 1 );
+  } );
+
+  it( 'existe 1 banner', async () => {
+    render( <App /> );
+    const banner = screen.getByRole('banner')
+    expect( banner ).toBeInTheDocument();
+  })
+
+} );
--- frontend/src/mockTest/testData.js
+++ frontend/src/mockTest/testData.js
@@ -1,3 +1,75 @@
-const data = [];
+//https://us-central1-squid-apis.cloudfunctions.net/test-front-basic
+const data = [
+  {
+    "_id": "59fa36b02108567eb3594a75",
+    "obtidoEm": "2018-01-11T19:06:13.751-02:00",
+    "origem": "instagram",
+    "uid": "1638715915171101208_1645525258",
+    "tags": [
+      "squider",
+      "squiders"
+    ],
+    "link": "https://www.instagram.com/p/Ba943FLjXoY/",
+    "tipo": "imagem",
+    "upvotes": 201,
+    "comentarios": 12,
+    "criadoEm": "2017-11-01T19:03:01.000-02:00",
+    "legenda": "Ontem foi dia de Halloween na Squid e todos entraram na brincadeira:\nteve gente fantasiada de Stranger Things, Power Rangers, super heróis, múmia, guarda de trânsito, sócios da Squid e muito mais.\n\nE isso mostra como nosso time e a nossa cultura organizacional se desenvolveu, e percebemos o quanto estamos felizes por estar trabalhando juntos! Sempre foi nosso objetivo cultivar a cultura #squider e é muito gratificante ver todos trabalhando pelo mesmo objetivo enquanto felizes e satisfeitos.\n\nObrigado, #squiders!",
+    "usuario": {
+      "id": "instagram|1645525258",
+      "username": "squiditapp",
+      "foto": "https://scontent.cdninstagram.com/vp/14c33e15329f3ad72df0c20f8ee03e8a/5BFE3330/t51.2885-19/s150x150/28427538_193161254748247_5960999047287275520_n.jpg",
+      "nome": "Squid"
+    },
+    "localizacao": null,
+    "imagens": {
+      "resolucaoPadrao": {
+        "url": "https://storage.googleapis.com/squid-story/1645525258/1638715915171101208_1645525258/image_resolucaoPadrao.jpg",
+        "width": 640,
+        "height": 426
+      },
+      "resolucaoMedia": {
+        "url": "https://storage.googleapis.com/squid-story/1645525258/1638715915171101208_1645525258/image_resolucaoMedia.jpg",
+        "width": 320,
+        "height": 213
+      },
+      "thumbnail": {
+        "url": "https://storage.googleapis.com/squid-story/1645525258/1638715915171101208_1645525258/image_thumbnail.jpg",
+        "width": 150,
+        "height": 150
+      }
+    },
+    "metadados": {
+      "filter": "Normal",
+      "users_in_photo": [
+        
+      ],
+      "engagement": 214,
+      "impressions": 1670,
+      "reach": 1218,
+      "saved": 1
+    },
+    "referencias": [
+      {
+        "status": "none",
+        "id": "59f74a7200cf4b17294019ad"
+      },
+      {
+        "isInfluencer": true,
+        "status": "none",
+        "type": "influencer-tag",
+        "id": "59dd047c6a06710012c571c8",
+        "oldId": "59dd07e6d0bc1e00110728e1"
+      },
+      {
+        "id": "5ac28e578138bb0012075702",
+        "tipo": "influencer-tag",
+        "status": "none",
+        "oldId": "59dd07e6d0bc1e00110728e1"
+      }
+    ],
+    "lastUpdate": "2018-08-22T05:02:12.655-03:00"
+  }
+];
 
 export default data;
